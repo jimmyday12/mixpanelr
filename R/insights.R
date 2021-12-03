453d7c419b97646218ff24a4046f741184a55474
jimmyday12 <jamesday@JDs-Macbook-Pro.brisbane.fusion.sport>
2021-12-03 11:36:44 +1100

    documenting function

--- R/insights.R
+++ R/insights.R
@@ -1,14 +1,15 @@
-
-#' Title
+#' Query saved insights report
 #'
-#' @param project_id
-#' @param bookmark_id
-#' @param workspace_id
-#' @param username
-#' @param secret
-#' @param eu_region
+#' Return data from a saved insights report in Mixpanel
 #'
-#' @return
+#' @param project_id required, an integer representing the id of the project
+#' @param bookmark_id required, bookmark_id of your Insights report can be found from the url: https://mixpanel.com/report/1/insights#report/<YOUR_BOOKMARK_ID>/example-report
+#' @param workspace_id optional, the id of the workspace if applicable.
+#' @param username required, service account username https://developer.mixpanel.com/reference/service-accounts
+#' @param secret required, service account secret https://developer.mixpanel.com/reference/service-accounts
+#' @param eu_region optional, set to TRUE if you are enrolled in the EU server residency
+#'
+#' @return returns a tibble with the data from the insights report
 #' @export
 #'
 #' @examples
@@ -20,10 +21,14 @@ mixpanel_insights <- function(project_id, bookmark_id, workspace_id = NULL,
                        bookmark_id = bookmark_id,
                        workspace_id = workspace_id,
                        username = username,
-                       secret = secret)
+                       secret = secret,
+                       eu_region = eu_region)
 
 
-  series <- resp %>%
+  json <- resp %>%
+    httr2::resp_body_json(flatten = TRUE)
+
+  series <- json %>%
     purrr::pluck("series") %>%
     purrr::map(purrr::flatten_dfr) %>%
     purrr::map_dfr(tidyr::pivot_longer, tidyr::everything(), names_to = "date",
